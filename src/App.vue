04f4cb2322efc3563977799aa537e47c38d5e466
mayandor <mayapologies6@gmail.com>
2020-10-07 18:57:25 -0400

    iniciando vue

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<!-- <template>
+  <v-container>
+    <v-layout wrap>
+      <v-flex class="grey lighten-5" xs6>
+        <h1>12</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template> -->
+
+<template>
+  <div>
+    <v-alert
+      border="top"
+      color="red lighten-2"
+      dark
+    >
+      I'm an alert with a top border and red color
+    </v-alert>
+    <v-alert
+      border="right"
+      color="blue-grey"
+      dark
+    >
+      I'm an alert with a right border and blue-grey color
+    </v-alert>
+    <v-alert
+      border="bottom"
+      color="pink darken-1"
+      dark
+    >
+      I'm an alert with a bottom border and pink color
+    </v-alert>
+    <v-alert
+      border="left"
+      color="indigo"
+      dark
+    >
+      I'm an alert with a border left type info
+    </v-alert>
+  </div>
+</template>
+<script>
+export default 
+{
+  
+}
+</script>
+
