4eac9d8e3189052a5666bfc66baa3cad6def2a86
mayandor <mayapologies6@gmail.com>
2020-10-07 20:12:12 -0400

    modificacion main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+//import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './node_modules/vuetify';
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
+
+//createApp(App).mount('#app')
 
 createApp(App).mount('#app')
