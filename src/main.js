e03550bde4db9fbf927d1a4f36b959b737047247
alexanderJPV <alexcyberay@gmail.com>
2020-10-07 20:14:33 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@
 //import { createApp } from 'vue'
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './node_modules/vuetify';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+var vuetify=new Vuetify({});
 
 new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
 
-//createApp(App).mount('#app')
-
-createApp(App).mount('#app')
+// esto ya no lo pongas => createApp(App).mount('#app')
